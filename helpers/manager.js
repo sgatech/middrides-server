d2f90f3473b61f60eea339ab270c4f250569cedc
TPeterW <peterwangtao0@hotmail.com>
2016-08-10 10:28:41 +0800

    Comments

--- helpers/manager.js
+++ helpers/manager.js
@@ -1,5 +1,12 @@
 const CONSTANTS = require("../config/constants");
-    
+
+/**
+ * Handle general server internal error
+ * 
+ * res {
+ *      error
+ * }
+ */
 function handleError(err, res) {
     console.log(err.message);
     res.status(500).json({
